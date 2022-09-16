0c37f833f62b26221cb7cd68c63f4c8b5041dd10
Kauri Kallaste <kaurikallaste@gmail.com>
2022-09-16 19:11:34 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+  	<h1>App lmao</h1>
+  </React.StrictMode>
+);
